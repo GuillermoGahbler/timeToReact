f81ae0ac482c08f6f74c3ce1b138dd27bbbc480e
Guillermo Gahbler <mexamericano@gmail.com>
2019-01-11 20:51:56 -0800

    added state;

--- time-to-react/src/App.js
+++ time-to-react/src/App.js
@@ -3,14 +3,38 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: ' Gericaya', age: 25 },
+      { name: ' Chamoya', age: 45 },
+      { name: ' Gallo', age: 42 }
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked')
+    //**don't do this -> */ this.state.persons[0].name='Maximilian'
+    this.setState({
+      persons: [
+        { name: ' Geemoe', age: 28 },
+        { name: ' Chamoya', age: 45 },
+        { name: ' Edisan', age: 42 }
+      ]
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <h1>React is now working</h1>
         <p>This is showing now</p>
-        <Person name='Javantay' age='20'/>
-        <Person name='Reactron' age='18'>Some of my hobbies are: Programming! </Person>
-        <Person name='Ceesharp' age='22'/>
+        <button onClick={this.switchNameHandler}>Switch names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name='Reactron' age=' 18 '>Some of my hobbies are: Programming! </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
 
--- time-to-react/src/Person/Person.js
+++ time-to-react/src/Person/Person.js
@@ -3,9 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
   <div>
-    <p>Testing random age generator for{props.name} person. Person is {
-    Math.floor(Math.random() * 30 +10)
-  } {props.age}years old
+    <p>Testing random age generator for{props.name} person. Person is {props.age}years old
   </p>
   <p>{props.children}</p>
   </div>)
