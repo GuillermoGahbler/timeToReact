a7d12466d989c75c36c0018b91e0e32cb646abb2
Guillermo Gahbler <mexamericano@gmail.com>
2019-01-14 13:43:43 -0800

    added style to button

--- time-to-react/src/App.js
+++ time-to-react/src/App.js
@@ -13,12 +13,12 @@ class App extends Component {
   }
 
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked')
     //**don't do this -> */ this.state.persons[0].name='Maximilian'
     this.setState({
       persons: [
-        { name: ' Geemoe', age: 28 },
+        { name: newName, age: 28 },
         { name: ' Thurgood', age: 38 },
         { name: ' Edisan', age: 100 }
       ]
@@ -26,27 +26,50 @@ class App extends Component {
   }
 
 
+  nameChangedHandler = (event)=>{
+    this.setState({
+      persons: [
+        { name: 'Horchata', age: 28 },
+        { name: event.target.value, age: 45 },
+        { name: ' Edisan', age: 100 }
+      ]
+    })
+  }
 
+  
 
   render() {
+
+    // this is inline styling sometimes its restricted to the scope
+    const style = {
+      backgroundColor:'white',
+      font:'inherit',
+      border:'1px dotted blue',
+      padding:'8px',
+      cursor:'pointer'
+    }
+
     return (
       <div className="App">
         <h1>React is now working</h1>
         <p>This is showing now</p>
-        <button onClick={this.switchNameHandler}>
+        <button style={style} 
+        onClick={() => this.switchNameHandler('moxy')}>
           Switch names & age
         </button>
-
+        {/* it's preferred to use bind as example below vs anon func above */}
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
-          click={this.switchNameHandler}
+          click={this.switchNameHandler.bind(this, 'Meezus')}
         />
 
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}>
+          click={this.switchNameHandler}
+          changed={this.nameChangedHandler}
+        >
           Some of my hobbies are: Programming!
         </Person>
 
--- time-to-react/src/Person/Person.js
+++ time-to-react/src/Person/Person.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
   return (
-  <div>
-    <p onClick={props.click}>Testing random age generator for{props.name} person. Person is {props.age}years old
-  </p>
-  <p>{props.children}</p>
-  </div>)
+    <div className='Person'>
+      <p onClick={props.click}> Testing random age generator for{props.name} person. Person is {props.age}years old </p>
+      
+      <p> {props.children} </p>
+
+      <input type="text" onChange={props.changed} value={props.name}/>
+
+
+    </div>
+    )
 };
 
 export default person;
